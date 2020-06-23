780926a010fbb882de71ebf74c7085e013d92f91
Steven Gutierrez <stematgut@gmail.com>
2020-06-23 12:20:13 -0700

    Created starter spring app with a models package

---
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/ReportsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ReportsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsServiceApplication.class, args);
+	}
+
+}
---
+++ p3backend/ReportsService/src/test/java/com/revature/ReportsService/ReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ReportsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
