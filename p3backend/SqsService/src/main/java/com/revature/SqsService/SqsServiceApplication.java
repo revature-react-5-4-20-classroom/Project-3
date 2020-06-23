9239cc0b40303d21245461b540fcdd9dd5324805
Mark Alsip <mark.alsip@comcast.net>
2020-06-23 14:34:43 -0600

    initializing services

---
+++ p3backend/SqsService/src/main/java/com/revature/SqsService/SqsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.SqsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsServiceApplication.class, args);
+	}
+
+}
