f0bc198c0f6b736a8c0296ecc0e4d91610d7a1ea
joel joseph <joeljva23@gmail.com>
2020-06-24 15:25:42 -0400

    skills

---
+++ p3backend/DataService/src/main/java/com/revature/DataService/models/SkillsDao.java
@@ -0,0 +1,12 @@
+package com.revature.DataService.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillsDao extends JpaRepository<Skills,Integer> {
+
+	
+	
+	
+	
+	
+}
