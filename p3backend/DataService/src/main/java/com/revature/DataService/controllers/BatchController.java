3ad46d5ed05d8347ceedced7202256e2a2bcf84a
Dan Brown <hereisdanbrown@gmail.com>
2020-06-24 15:08:55 -0600

    Moved all of group3 dataservice packages

---
+++ p3backend/DataService/src/main/java/com/revature/DataService/controllers/BatchController.java
@@ -0,0 +1,45 @@
+package com.revature.DataService.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import com.revature.DataService.models.Batch;
+import com.revature.DataService.services.BatchService;
+
+@RestController
+public class BatchController {
+	@Autowired
+	BatchService batchService;
+	
+	@GetMapping("/batches")
+	public List<Batch> getAllBatches() {
+	  return batchService.getAll();
+	}
+	
+	@GetMapping("/batches/{id}")
+	public Batch getBatchById(@PathVariable Integer id) {
+	  try {
+        return batchService.getById(id);
+      } catch (Exception e) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+      }
+	}
+	
+	@PatchMapping("batches/{id}")
+	public Batch updateBatchWithId(@RequestBody Batch batch, @PathVariable Integer id) {
+	  batch.setBatchId(id);
+	  try {
+        return batchService.updateBatch(batch);
+      } catch (Exception e) {
+          throw new ResponseStatusException(HttpStatus.CONFLICT);
+      }
+	}
+	
+	
+}
---
+++ p3backend/DataService/src/main/java/com/revature/DataService/models/Batch.java
@@ -0,0 +1,58 @@
+package com.revature.DataService.models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.revature.DataService.models.Trainer;
+
+@Entity
+@Table(schema = "project3", name = "batch")
+public class Batch {
+
+	public Batch() {
+		super();
+		
+	}
+	
+	@Id
+	@Column(name="batch_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer batchId;
+	
+	@Column(name="start_date")
+	private Date startDate;
+	
+	@Column(name="end_date")
+	private Date endDate;
+	
+	@Column(name="isconfirmed")
+	private Boolean isConfirmed;
+	
+	@Column(name="interview_score_lower")
+	private Integer interviewScoreLower;
+	
+	@JoinColumn(name="trainer_id")
+	@OneToOne(fetch = FetchType.EAGER)
+	// May need a JsonIgnoreProperties later on
+	private Trainer trainer;
+	
+
+	
+	
+//	@JoinColumn(name="location_id")
+//	@ManyToOne(fetch = FetchType.EAGER)
+	
+	
+	
+	
+}
