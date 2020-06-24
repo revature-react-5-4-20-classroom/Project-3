5138fcab357c467bc3d07ef5849546adc9d64d5e
Daniel Corkran <dwcor123@uab.edu>
2020-06-24 14:21:18 -0500

    added skillset model and basic get endpoints

---
+++ p3backend/DataService/src/main/java/com/revature/DataService/models/Skillset.java
@@ -0,0 +1,80 @@
+package com.revature.DataService.models;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "project3",name = "skillset")
+public class Skillset {
+  
+  @Id
+  @Column(name = "skillset_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer skillSetId;
+  
+  @Column(name = "name")
+  private String skillSetName;
+  
+  @ManyToMany
+  @JoinTable(
+      name = "skillset_skills",
+      joinColumns = @JoinColumn(name = "skillset_id",referencedColumnName = "skillset_id"),
+      inverseJoinColumns = @JoinColumn(name = "skill_id",referencedColumnName = "skill_id"))
+  private List<Skill> skills;
+  
+  
+  @ManyToMany(mappedBy = "skillsets")
+  private List<Curriculum> curricula;
+  
+  public Skillset() {
+    super();
+  }
+
+  public Skillset(Integer skillSetId, String skillSetName) {
+    super();
+    this.skillSetId = skillSetId;
+    this.skillSetName = skillSetName;
+  }
+
+  public Integer getSkillSetId() {
+    return skillSetId;
+  }
+
+  public void setSkillSetId(Integer skillSetId) {
+    this.skillSetId = skillSetId;
+  }
+
+  public String getSkillSetName() {
+    return skillSetName;
+  }
+
+  public void setSkillSetName(String skillSetName) {
+    this.skillSetName = skillSetName;
+  }
+
+  public List<Skill> getSkills() {
+    return skills;
+  }
+
+  public void setSkills(List<Skill> skills) {
+    this.skills = skills;
+  }
+
+  public List<Curriculum> getCurricula() {
+    return curricula;
+  }
+
+  public void setCurricula(List<Curriculum> curricula) {
+    this.curricula = curricula;
+  }
+
+  
+}
---
+++ p3backend/DataService/src/main/java/com/revature/DataService/services/SkillSetService.java
@@ -0,0 +1,24 @@
+package com.revature.DataService.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.revature.DataService.models.Skillset;
+import com.revature.DataService.repositories.SkillSetRepository;
+
+@Service
+public class SkillSetService {
+  
+  @Autowired 
+  SkillSetRepository skillSetRepository;
+  
+  public List<Skillset> getAll() {
+    return skillSetRepository.findAll();
+  }
+  
+  public Skillset getById(Integer id) {
+    Optional<Skillset> skillset = skillSetRepository.findById(id);
+    return skillset.get();
+  }
+}
