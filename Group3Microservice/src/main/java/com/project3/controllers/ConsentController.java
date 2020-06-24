83cab44a63616cd80c1ed0c790563edf8a5ccc1b
steenn30 <steenn30@gmail.com>
2020-06-24 12:21:53 -0400

    consent service

---
+++ Group3Microservice/src/main/java/com/project3/controllers/ConsentController.java
@@ -0,0 +1,44 @@
+package com.project3.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project3.models.Consent;
+import com.project3.services.ConsentService;
+
+
+@RestController
+public class ConsentController {
+
+	
+	@Autowired
+	ConsentService consentService;
+	
+	// get all items
+		@CrossOrigin(origins = "*")
+		@GetMapping("/consent")
+		public List<Consent> getAllItems() {
+			return consentService.getAll();
+		}
+		
+		@CrossOrigin(origins="*")
+		@PatchMapping("/consent")
+		public Consent updateConsentApproval(@RequestBody Consent consent) {
+			return consentService.update(consent);
+		}
+		
+		@CrossOrigin(origins="*")
+		@PostMapping("/consent")
+		public Consent createConsent(@RequestBody Consent consent) {
+			return consentService.create(consent);
+		}
+	
+	
+}
---
+++ Group3Microservice/src/main/java/com/project3/services/ConsentService.java
@@ -0,0 +1,30 @@
+package com.project3.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project3.models.Consent;
+import com.project3.repositories.ConsentRepository;
+
+
+@Service
+public class ConsentService {
+
+	
+	@Autowired
+	ConsentRepository consentRepository;
+	
+	public List<Consent> getAll(){
+		return consentRepository.findAll();
+	}
+	
+	public Consent create(Consent consent) {
+		return consentRepository.save(consent);
+	}
+	
+	public Consent update(Consent consent) {
+		return consentRepository.save(consent);
+	}
+}
