132472ba8001f832553fc9ee40944b14df3e5256
steenn30 <steenn30@gmail.com>
2020-06-25 13:53:59 -0400

    @Repository

--- p3backend/DataService/src/main/java/com/revature/DataService/repositories/CurriculumRepository.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/repositories/CurriculumRepository.java
@@ -1,9 +1,11 @@
 package com.revature.DataService.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.DataService.models.Curriculum;
 
+@Repository
 public interface CurriculumRepository extends JpaRepository<Curriculum,Integer> {
 
 }
