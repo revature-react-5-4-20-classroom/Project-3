6a8d1cb5b9be6d47853a9d24922c102d0b7ef599
Dan Brown <hereisdanbrown@gmail.com>
2020-06-25 10:37:35 -0600

    Finally got the skill set and skills many to many functioning. Also got the trainer skill set relationship working

--- p3backend/DataService/src/main/java/com/revature/DataService/repositories/BatchRepository.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/repositories/BatchRepository.java
@@ -1,11 +1,12 @@
 package com.revature.DataService.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import com.revature.DataService.models.Batch;
 
 
+@Repository
 public interface BatchRepository extends JpaRepository<Batch, Integer>{
 
 }
--- p3backend/DataService/src/main/java/com/revature/DataService/repositories/TrainerRepository.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/repositories/TrainerRepository.java
@@ -1,10 +1,12 @@
 package com.revature.DataService.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.DataService.models.Trainer;
 
 
+@Repository
 public interface TrainerRepository extends JpaRepository<Trainer, Integer> {
 
 }
