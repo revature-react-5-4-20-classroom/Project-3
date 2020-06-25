357fe25cb3ed4229f4e7f105a27fa981fee7d580
Liam Allard <lallard13@gmail.com>
2020-06-25 09:48:02 -0700

    Added client model and endpoints - set up multiplicity later

---
+++ p3backend/DataService/src/main/java/com/revature/DataService/models/Client.java
@@ -0,0 +1,70 @@
+package com.revature.DataService.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(schema="project3", name="client")
+public class Client {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="client_id")
+	private Integer clientId;
+	
+	@Column(name="name")
+	private String name;
+	
+	// Multiplicity
+	// Note: should skillset/client be one to many?
+	@OneToOne // Skillset should have mappedBy="client"
+	@JsonIgnoreProperties({"client"})
+	@Column(name="client_skillset_id")
+	private Skillset clientSkillset;
+	
+//	@OneToOne(mappedBy="clientdemand") // Note: I feel that this should be reversed, but it likely doesn't matter 
+//	@JsonIgnoreProperties({"client"})
+//	private ClientDemand clientDemand;
+
+	public Integer getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(Integer clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Skillset getClientSkillset() {
+		return clientSkillset;
+	}
+
+	public void setClientSkillset(Skillset clientSkillset) {
+		this.clientSkillset = clientSkillset;
+	}
+
+//	public ClientDemand getClientDemand() {
+//		return clientDemand;
+//	}
+//
+//	public void setClientDemand(ClientDemand clientDemand) {
+//		this.clientDemand = clientDemand;
+//	}
+	
+	
+
+}
