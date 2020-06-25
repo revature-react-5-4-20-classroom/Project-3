cd61e8bc80606b122b725467885554caa08debeb
Steven Gutierrez <stematgut@gmail.com>
2020-06-25 09:16:14 -0700

    added service/mockservice for Client

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/service/ClientService.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/service/ClientService.java
@@ -1,5 +1,21 @@
 package com.revature.ReportsService.service;
 
+import java.util.List;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import com.revature.ReportsService.models.Client;
+
+@Service
+@Primary
 public class ClientService {
 
+  public List<Client> getAll() {
+    // return openfigen grab to the dataserve endpoint for all
+    return new MockClient().getClients();
+  }
+  
+  public Client getById(Integer id) {
+    // return openfeign grab to the dataservde endpoint for one
+    return new MockClient().getClientById(id);
+  }
 }
