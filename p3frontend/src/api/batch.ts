11291c3317f500e563637576e57eb0f46a5d5600
J-Agens <johntagens@gmail.com>
2020-06-26 14:44:02 -0400

    moved assignTrainer() to api/bacth.ts, re-added PATCH to /batches/{id} in controller

--- p3frontend/src/api/batch.ts
+++ p3frontend/src/api/batch.ts
@@ -10,11 +10,10 @@ export async function getBatchById(batchId:number ){
     }
 }
 
-// Confirm/Unconfirm a batch // Assigm, unassign, or switch batch trainers
-// NEEDS TO BE UPDATED
-export async function updateBatch(bId : number, isConf : boolean, trainId? : number) : Promise<Batch> {
+// Confirm/Unconfirm a batch 
+export async function updateBatch(bId : number, isConf : boolean) : Promise<Batch> {
     try {
-        const dataTransfer =  {batchId: bId, isConfirmed: isConf, trainerId: trainId ? trainId : null};
+        const dataTransfer =  {batchId: bId, isConfirmed: isConf};
         const response = await storeClient.patch(`/batches/${bId}`, dataTransfer);
         const {
             batchId, 
@@ -43,3 +42,16 @@ export async function updateBatch(bId : number, isConf : boolean, trainId? : num
         throw e;
     }
 }
+
+// Assign trainer to a batch (currently returns void)
+export async function assignTrainer(bId : number, trainId : number) : Promise<void> {
+    try {
+        const dataTransfer = { batchId: bId, trainerId: trainId };
+        const response = await storeClient.post('/trainerbatch', dataTransfer);
+        console.log(`Trainer ${trainId} assigned to batch ${bId}`);
+    } catch (e) {
+        console.log('failed to assign trainer to batch', e.message);
+        throw e;
+    }
+}
+
