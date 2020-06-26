26546c9f5a369cbd73d0ae1e22ba8ecdb1aa569a
kruti4 <krutifaldu9@gmail.com>
2020-06-26 13:07:35 -0700

    frontend page structure for showing and editing batches

--- p3frontend/src/Components/Batch/batchList.tsx
+++ p3frontend/src/Components/Batch/batchList.tsx
@@ -1,11 +1,72 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Nav, NavItem, NavLink } from "reactstrap";
+import { Route } from "react-router";
+import { BatchView } from "./BatchView";
+// import { NavLink } from "react-router-dom";
 
 export class BatchList extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: false,
+    };
+  }
+  batchData = (e: any) => {
+    e.preventDefault();
+    this.setState({
+      data: true,
+    });
+  };
   render() {
     return (
       <Container style={{ backgroundColor: "#474c55" }}>
-        <h3>ligvam;sri</h3>
+        <h3>Possible Batches</h3>
+        {/* {this.state.comments.map((obj: any, index: number) => {
+          return  <NavItem>
+            <NavLink href="#" onClick={this.batchData}>
+              Carriculam 1
+            </NavLink>
+          </NavItem>
+        })} */}
+        <Nav pills>
+          <NavItem>
+            <NavLink href="#" onClick={this.batchData}>
+              Carriculam 1
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" onClick={this.batchData}>
+              Carriculam 2
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" onClick={this.batchData}>
+              Carriculam 3
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#" onClick={this.batchData}>
+              Carriculam 4
+            </NavLink>
+          </NavItem>
+        </Nav>
+        {/* just for getting ideas */}
+        <div style={{ display: this.state.data ? "block" : "none" }}>
+          <BatchView />
+          <br />
+          <BatchView />
+          <br />
+          <BatchView />
+          <br />
+          <BatchView />
+          <br />
+          <BatchView />
+          <br />
+        </div>
+
+        {/* {this.state.comments.map((obj: any, index: number) => {
+          return <BatchView></BatchView>;
+        })} */}
       </Container>
     );
   }
--- p3frontend/src/Components/Batch/options.tsx
+++ p3frontend/src/Components/Batch/options.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Input, Label, Button } from "reactstrap";
 
 export class Options extends React.Component<any, any> {
   render() {
-    return <Container style={{backgroundColor:"#474c55"}}>
-    <h4>Options</h4>
-    </Container>;
+    return (
+      <Container style={{ backgroundColor: "#474c55" }}>
+        <h4>Options</h4>
+        <Label>something:</Label>
+        <Input></Input>
+        <Button>Submit</Button>
+        <br />
+        <br />
+        <Button>Generate Batches</Button>
+      </Container>
+    );
   }
 }
