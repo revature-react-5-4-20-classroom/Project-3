d498b2e749c0cc0fabb6dea1e9490de9c6b2880c
Steven Gutierrez <stematgut@gmail.com>
2020-06-26 13:03:21 -0700

    Started to work on batch generation algorithm provided by Kruti.  Realized we need to populate all of our models with fake data - will be doing this this weekend

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/service/MockBatch.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/service/MockBatch.java
@@ -1,19 +1,27 @@
 package com.revature.ReportsService.service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.revature.ReportsService.models.Batch;
+import com.revature.ReportsService.models.ClientDemand;
 
 @Service
 public class MockBatch {
 
 	private List<Batch> batches;
+    private Integer nextId;
 
 	public MockBatch() {
 		super();
-		// insert fake data here
+//        this.batches = new ArrayList<Batch>();
+//        this.batches.add(new Batch(1, 2, LocalDate.parse("2020-08-01"), 1, 1));
+//        this.batches.add(new Batch(2, 3, LocalDate.parse("2020-08-01"), 2, 2));
+//        this.batches.add(new Batch(3, 5, LocalDate.parse("2020-08-01"), 3, 3));
+//        this.nextId = 4;
 	}
 
 	public List<Batch> getBatches() {
