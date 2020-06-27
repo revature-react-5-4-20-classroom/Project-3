2648ed3fb32f174c3251e880e4cf2c6324fdae2a
Dianne Desilets <deedesilets@yahoo.com>
2020-06-27 16:00:24 -0400

    something renders but no data loading

--- p3backend/DataService/src/main/java/com/revature/DataService/controllers/AssociateController.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/controllers/AssociateController.java
@@ -1,13 +1,28 @@
 package com.revature.DataService.controllers;
 
+import com.revature.DataService.models.Associate;
+import com.revature.DataService.services.AssociateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.DataService.services.AssociateService;
-
-
 @RestController
+
 public class AssociateController {
 	@Autowired
 	AssociateService associateService;
+	
+	@GetMapping("/associates")
+	public Associate[] getAssociates() {
+		
+		return associateService.getAllAssociates();
+	}
+	
+	@PostMapping("/associates")  
+	public void updateAssociateBatch (@RequestBody Integer associateId, @RequestBody Integer assignedBatchId) {
+		associateService.updateAssociateBatch(associateId, assignedBatchId);
+	}
+	
 }
--- p3backend/DataService/src/main/java/com/revature/DataService/services/AssociateService.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/services/AssociateService.java
@@ -3,11 +3,23 @@ package com.revature.DataService.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.DataService.models.Associate;
 import com.revature.DataService.repositories.AssociateRepository;
-
 
 @Service
 public class AssociateService {
 	@Autowired
 	AssociateRepository associateRepository;
+	
+	public Associate[] getAllAssociates()  {
+		
+		return (Associate[])(associateRepository.findAll()).toArray();
+	}
+	
+public void updateAssociateBatch(Integer associateId, Integer assignedBatchId) {
+	associateRepository.update(associateId, assignedBatchId);
 }
+	
+	
+}
+
