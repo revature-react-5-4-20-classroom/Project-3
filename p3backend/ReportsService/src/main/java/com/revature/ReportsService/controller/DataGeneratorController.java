d23081d3bc9b4d7f909dfab15358f0a8470cd4c2
Mark Alsip <mark.alsip@comcast.net>
2020-06-30 07:33:56 -0600

    solving merge conflicts

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
@@ -1,25 +1,19 @@
 package com.revature.ReportsService.controller;
 
-<<<<<<< HEAD
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.ReportsService.DataGetterFeign;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
->>>>>>> f5daf7a519f9f3b3608599fa37162b4d13ecabd8
 @RestController
 public class DataGeneratorController {
-	@Autowired
-	DataGetterFeign dataGetterFeign;
-
-	@GetMapping("/DataGenerator")
-	public String getAssociatesData() {
-		return "data generator" + dataGetterFeign.getData();
-	}
+//	@Autowired
+//	DataGetterFeign dataGetterFeign;
+//
+//	@GetMapping("/DataGenerator")
+//	public String getAssociatesData() {
+//		return "data generator" + dataGetterFeign.getData();
+//	}
 
 	@GetMapping("/hello")
 	public String hello() {
