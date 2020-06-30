8c2c63b02e169473fdd2b7d3dd3c426273fed35a
kruti4 <krutifaldu9@gmail.com>
2020-06-29 23:32:36 -0700

    added feign client files -testing remaining

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
@@ -1,5 +1,18 @@
 package com.revature.ReportsService.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.ReportsService.DataGetterFeign;
+@RestController
 public class DataGeneratorController {
+	@Autowired
+	DataGetterFeign dataGetterFeign;
+
+	@GetMapping("/DataGenerator")
+	public String getAssociatesData() {
+		return "data generator" + dataGetterFeign.getData();
+	}
 
 }
