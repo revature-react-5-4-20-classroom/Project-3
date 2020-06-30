45d3ee7d6927b1df5dd83f571868601b7c411a4c
Steven Gutierrez <stematgut@gmail.com>
2020-06-30 08:25:49 -0700

    Added comments to describe what is happening

--- p3frontend/src/Story2/colGraphComponent.tsx
+++ p3frontend/src/Story2/colGraphComponent.tsx
@@ -25,19 +25,25 @@ export class ColumnChartTest extends React.Component<any, any> {
     }
   }
 
+  // This initializes google charts
   loadGoogle = () => {
     google.charts.load('current', { packages: ['corechart', 'bar'] });
     google.charts.setOnLoadCallback(this.drawChart);
   };
 
+  // Method to create a chart/table
   drawChart = () => {
+    // Creating a data obj for our table
     var data = new google.visualization.DataTable();
+    // Number Columns represent each type of data - 5 total
+    // String column represent a title
     data.addColumn('string', 'Demand and Supply');
     data.addColumn('number', 'Total');
     data.addColumn('number', 'Java/React');
     data.addColumn('number', 'Salesforce');
     data.addColumn('number', 'Data');
     data.addColumn('number', 'Ai');
+    // Each array is a grouping of the above columns
     data.addRows([
       ['Client Demand', 64, 23, 21, 16, 4],
       ['Current', 19, 4, 6, 8, 1],
@@ -45,9 +51,11 @@ export class ColumnChartTest extends React.Component<any, any> {
       ['3 Months', 50, 20, 15, 10, 5],
     ]);
 
+    // Creates view with data?????????
     var view = new google.visualization.DataView(data);
     view.setRows([0, 1, 2, 3]);
 
+    // Labeling and styling
     var options: any = {
       orientation: 'horizontal',
       width: 900,
@@ -64,8 +72,11 @@ export class ColumnChartTest extends React.Component<any, any> {
       },
     };
 
+    // myRef acts like .getElementById but for React
     var chart = new google.visualization.BarChart(this.myRef.current);
+    // This is what actually creates the table
     chart.draw(view, options);
+    // Updates componment once chart is drawn
     this.setState({
       shouldUpdate: true,
     });
