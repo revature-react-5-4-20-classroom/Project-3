44e5a8881a6e89531048695f8e0e156f64ccfc39
Mark Alsip <mark.alsip@comcast.net>
2020-06-30 09:44:58 -0600

    still working on getting the jenkins server going

--- p3frontend/src/App.tsx
+++ p3frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 import { BrowserRouter as Router, Route, Switch, BrowserRouter, NavLink } from 'react-router-dom';
-import { InProgress, ReduxInProgress } from './Story1/InProgress';
+import { InProgress } from './Story1/InProgress';
 import { Navbar, NavbarToggler, Nav, NavItem, Container } from 'reactstrap';
 
 import { TrainerAssignmentComponent } from './Components/TrainerAssignment';
@@ -10,16 +11,7 @@ import { OverviewClientDemand } from './Story2/OverviewClientDemand';
 import { OverviewTraining } from './Story3/OverviewTraining';
 import { AssignTrainer } from './Story4/AssignTrainer';
 import { TestdateDifferenceWeeks } from './GeneralPurposeHelpers/dateDifferenceWeeks';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
 
-import { TrainerAssignmentComponent } from "./Components/TrainerAssignment";
-import { ViewConsentRequests } from "./Components/ViewConsentRequests";
-import { OverviewClientDemand } from "./Story2/OverviewClientDemand";
-import { OverviewTraining } from "./Story3/OverviewTraining";
-import { AssignTrainer } from "./Story4/AssignTrainer";
-import { TestdateDifferenceWeeks } from "./GeneralPurposeHelpers/dateDifferenceWeeks";
-import { BatchPage } from "./Components/Batch/BatchPage";
 
 export class App extends React.Component<any, any> {
   constructor(props: any) {
@@ -31,32 +23,31 @@ export class App extends React.Component<any, any> {
     setIsOpen(!isOpen);
   };
 
-  render() {
-    return (
-      <Container>
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        />
-        {/* <TestdateDifferenceWeeks/> */}
-        <Router>
-          <Navbar color="light" light expand="md">
-            <NavbarToggler onClick={this.toggleNavbar} />
-            <Nav className="mr-auto" tabs>
+
+  render()
+  {
+    return (<Container>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+      {/* <TestdateDifferenceWeeks/> */}
+      <Router>
+        <Navbar color='light' light expand='md'>
+            <NavbarToggler onClick={this.toggleNavbar}/>
+            <Nav className='mr-auto' tabs>
               <NavItem>
                 <NavLink
-                  to="/home"
-                  className="nav-link"
-                  activeClassName="active"
+                  to='/home'
+                  className='nav-link'
+                  activeClassName='active'
                 >
                   Home
                 </NavLink>
               </NavItem>
               <NavItem>
+
                 <NavLink
-                  to="/batches"
-                  className="nav-link"
-                  activeClassName="active"
+                  to='/batches'
+                  className='nav-link'
+                  activeClassName='active'
                 >
                   Batches
                 </NavLink>
@@ -65,60 +56,37 @@ export class App extends React.Component<any, any> {
                 <NavLink to='/in-progress' className='nav-link' activeClassName='active'>In Progress</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink
-                  to="/overview"
-                  className="nav-link"
-                  activeClassName="active"
-                >
-                  Overview
-                </NavLink>
+                <NavLink to='/overview' className='nav-link' activeClassName='active'>Overview</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink
-                  to="/overview-training"
-                  className="nav-link"
-                  activeClassName="active"
-                >
-                  Training Overview
-                </NavLink>
+                <NavLink to='/overview-training' className='nav-link' activeClassName='active'>Training Overview</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink
-                  to="/assign-trainer"
-                  className="nav-link"
-                  activeClassName="active"
-                >
-                  Assign Trainers
-                </NavLink>
+                <NavLink to='/assign-trainer' className='nav-link' activeClassName='active'>Assign Trainers</NavLink>
               </NavItem>
             </Nav>
         </Navbar>
-        <Switch>
-          <Provider store={store}>
-            <Route path='/home'>
-              Home page
-            </Route>
-            <Route path='/in-progress'>
-              <ReduxInProgress/>
-            </Route>
-            <Route path='/overview'>
-              <OverviewClientDemand/>
-            </Route>
-            <Route path='/overview-training'>
-              <OverviewTraining/>
-            </Route>
-            <Route path='/assign-trainer'>
-              <AssignTrainer/>
-              <TrainerAssignmentComponent/>
-            </Route>
-            <Route path='/trainers'>
-              <TrainerAssignmentComponent />
-            </Route>
-            <Route path='/consent'>
-              <ViewConsentRequests />
-            </Route>
-           </Provider>
-          </Switch>
+        <Route path='/home'>
+          Home page
+        </Route>
+        <Route path='/in-progress'>
+          <InProgress/>
+        </Route>
+        <Route path='/overview'>
+          <OverviewClientDemand/>
+        </Route>
+        <Route path='/overview-training'>
+          <OverviewTraining/>
+        </Route>
+        <Route path='/assign-trainer'>
+          <AssignTrainer/>
+        </Route>
+        <Route path='/trainers'>
+          <TrainerAssignmentComponent />
+        </Route>
+        <Route path='/consent'>
+          <ViewConsentRequests />
+        </Route>
       </Router>
     </Container>)
 
