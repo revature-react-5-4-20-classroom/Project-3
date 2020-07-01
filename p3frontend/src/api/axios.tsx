d8d50de92bb6ed42748ef9b11974bd2c8a897d8e
Mark Alsip <mark.alsip@comcast.net>
2020-07-01 14:29:07 -0600

    changed ports

--- p3frontend/src/api/axios.tsx
+++ p3frontend/src/api/axios.tsx
@@ -1,8 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 
+//GATEWAY MASTER PORT IS 2331 - SWAP THIS IN WHEN PUSHING TO MASTER
+//GATEWAT DEVELOPMENT PORT IS 8212 - THIS SHOULD BE ACTIVE BY DEFAULT
+
 export const axiosClient = axios.create({
   // baseURL : 'http://localhost:1235', // Use this to test on your local machine, leave commented out.
-  baseURL: 'http://3.235.74.191:8212/data',
+  baseURL: 'http://3.235.74.191:2331/data',
   //if you don't have the following line, your login won't work
   withCredentials: false, // we should probably change this later
 });
