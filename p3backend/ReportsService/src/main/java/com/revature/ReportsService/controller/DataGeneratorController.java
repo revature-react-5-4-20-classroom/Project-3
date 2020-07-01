48835ff7d19ff4a32f873c6325766d8ed66c06be
kruti4 <krutifaldu9@gmail.com>
2020-07-01 08:49:18 -0700

    working feign client on localmachine

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/controller/DataGeneratorController.java
@@ -1,15 +1,28 @@
 package com.revature.ReportsService.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.ReportsService.DataGetterFeign;
 
 @RestController
 public class DataGeneratorController {
 
+	@Autowired
+	DataGetterFeign dataGetterFeign;
+
+	@GetMapping("datagetter")
+	public String getData() {
+
+		return "Retrieved from appbook service: " + dataGetterFeign.getAssociates();
+
+	}
+
 	@GetMapping("/hello")
 	public String hello() {
-		
+
 		return "hello frontend";
 	}
-	
+
 }
