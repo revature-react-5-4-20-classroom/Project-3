43800f8ce14700e9ef69915b7e906b142d01b3e8
malsip <31218596+malsip@users.noreply.github.com>
2020-07-02 09:21:30 -0600

    Update DataServiceApplication.java

--- p3backend/DataService/src/main/java/com/revature/DataService/DataServiceApplication.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/DataServiceApplication.java
@@ -6,9 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -17,15 +16,26 @@ public class DataServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataServiceApplication.class, args);
 	}
-
 	@Bean
-	public WebMvcConfigurer configureContent() {
+	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-				configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+				.allowedHeaders("*");
+				
 			}
 		};
+	}
+	
+	@Bean
+    public WebMvcConfigurer configureContent() {
+      return new WebMvcConfigurer() {
+        @Override
+        public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+          configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+        }
+      };
 	}
 
 }
