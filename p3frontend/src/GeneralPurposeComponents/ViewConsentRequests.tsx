66ed5f008f45d34c5388204cf421e2a2345713d3
unknown <renecouture26@gmail.com>
2020-07-02 10:56:14 -0400

    formatted some code. fixed one error.

--- p3frontend/src/GeneralPurposeComponents/ViewConsentRequests.tsx
+++ p3frontend/src/GeneralPurposeComponents/ViewConsentRequests.tsx
@@ -1,83 +1,93 @@
-import React from 'react';
-import {approveConsentRequest, denyConsentRequest, getConsentByTrainerId} from '../api/consent'
-import { Consent } from '../models/Consent';
-import {  ListGroup, ListGroupItem, Button} from 'reactstrap';
+import React from "react";
+import {
+  approveConsentRequest,
+  denyConsentRequest,
+  getConsentByTrainerId,
+} from "../api/consent";
+import { Consent } from "../models/Consent";
+import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 interface IViewConsentRequestsState {
-    consentRequests:Consent[];
+  consentRequests: Consent[];
 }
-export class ViewConsentRequests extends React.Component<any,IViewConsentRequestsState>{
+export class ViewConsentRequests extends React.Component<
+  any,
+  IViewConsentRequestsState
+> {
+  constructor(props: any) {
+    super(props);
 
+    this.state = {
+      consentRequests: [],
+    };
+  }
 
-    constructor(props:any){
-        super(props);
+  componentDidMount() {
+    this.getConsentRequests();
+  }
 
-        this.state = {
-            consentRequests : []
-        }
-    }
+  accept = async (id: number) => {
+    let consentRequest: Consent = this.state.consentRequests[id];
+    consentRequest.isApproved = true;
+    await approveConsentRequest(consentRequest);
+  };
 
-    componentDidMount(){
-        this.getConsentRequests();
-    }
+  decline = async (id: number) => {
+    let consentRequest: Consent = this.state.consentRequests[id];
+    consentRequest.isApproved = false;
+    await denyConsentRequest(consentRequest);
+  };
 
-    accept = async(id:number) =>{
-        let consentRequest : Consent= this.state.consentRequests[id];
-        consentRequest.isApproved = true;
-        await approveConsentRequest(consentRequest);
-    }
+  getConsentRequests = async () => {
+    let consentRequests: Consent[] = await getConsentByTrainerId(1);
 
-    decline = async(id:number) =>{
-        let consentRequest : Consent = this.state.consentRequests[id];
-        consentRequest.isApproved = false;
-        await denyConsentRequest(consentRequest);
-    }
+    this.setState({
+      consentRequests: consentRequests,
+    });
+  };
+  render() {
+    return (
+      <>
+        <h6>View Consent Requests</h6>
+        <ListGroup>
+          {this.state.consentRequests.map((consent: Consent, i) => {
+            //trying to use the same item display everywhere
+            return (
+              <ListGroupItem key={i}>
+                {consent.batchId}
+                <Button
+                  color="primary"
+                  id={i.toString()}
+                  onClick={() => this.accept(i)}
+                >
+                  Accept
+                </Button>
+                <Button
+                  color="primary"
+                  id={i.toString()}
+                  onClick={() => this.decline(i)}
+                >
+                  Decline
+                </Button>
+              </ListGroupItem>
+            );
 
-    getConsentRequests = async () => {
-        let consentRequests: Consent[] = await getConsentByTrainerId(1);
+            // <ListGroupItem key={i}>
+            //     <Row>
+            //         <Col xs='auto'><img src={getImageUrl(this.state.itemList[i])} style={{height:"100px", width:"auto"}} /></Col>
+            //         <Col xs='auto'><a href='#' onClick={this.toggleRedirect} id={i.toString()}>{item.item_name}</a></Col>
 
-        this.setState({
-            consentRequests : consentRequests
-        })
-            
-        
-    }
-    render(){
-        return(
-            <>
-            <h6>View Consent Requests</h6>
-                    <ListGroup>
-                            {this.state.consentRequests.map((consent: Consent, i) => {
-                                
-                                //trying to use the same item display everywhere
-                                return( 
-                                <ListGroupItem key={i}>
-                                    
-                                    
-                                    
-                                        {consent.batchId}
-                                        <Button color="primary" id={i.toString()} onClick={()=>this.accept(i)}>Accept</Button>
-                                        <Button color="primary" id={i.toString()} onClick={()=>this.decline(i)}>Decline</Button>
-                                    
-                                
-                                </ListGroupItem>)
+            //         {/* Added this so long descriptions did not go off page */}
+            //         <Container>
+            //         <Col xs='auto'>{item.description}</Col>
+            //         </Container>
 
-                                // <ListGroupItem key={i}>
-                                //     <Row>
-                                //         <Col xs='auto'><img src={getImageUrl(this.state.itemList[i])} style={{height:"100px", width:"auto"}} /></Col>
-                                //         <Col xs='auto'><a href='#' onClick={this.toggleRedirect} id={i.toString()}>{item.item_name}</a></Col>
-
-                                //         {/* Added this so long descriptions did not go off page */}
-                                //         <Container>
-                                //         <Col xs='auto'>{item.description}</Col>
-                                //         </Container>
-
-                                //         <Col xs='auto'><Button color="primary" id={i.toString()} onClick={this.addToCart}>Add to cart</Button></Col>
-                                //     </Row>
-                                //</ListGroupItem>
-                                
-                            })}
-                        </ListGroup></>
-        )
-    }
+            //         <Col xs='auto'><Button color="primary" id={i.toString()} onClick={this.addToCart}>Add to cart</Button></Col>
+            //     </Row>
+            //</ListGroupItem>
+          })}
+        </ListGroup>
+      </>
+    );
+  }
 }
--- p3frontend/src/GeneralPurposeHelpers/Prnt.tsx
+++ p3frontend/src/GeneralPurposeHelpers/Prnt.tsx
@@ -5,11 +5,14 @@
 	text:string		the text to print
 	dataJson?:any	a possible JSON to print aswell
 */
-export function prnt(debug:boolean,text:string,dataJson?:any)
-{
-    if(debug) 
-    {
-        if(dataJson)    console.log(`${text} ${JSON.stringify(dataJson)}`)
-        else            console.log(text)
-    }
+export function prnt(debug: boolean, text: string, dataJson?: any) {
+  if (debug) {
+    if (dataJson) {
+      try {
+        console.log(`${text} ${JSON.stringify(dataJson)}`);
+      } catch (e) {
+        console.log(`prnt error ${e.message}`);
+      }
+    } else console.log(text);
+  }
 }
