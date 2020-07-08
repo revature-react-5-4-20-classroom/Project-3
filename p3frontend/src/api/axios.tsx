f22b4561f3115bcd7e4cf5a93bae067f94c8d555
steenn30 <steenn30@gmail.com>
2020-07-08 10:44:38 -0400

    persistence changes

--- p3frontend/src/api/axios.tsx
+++ p3frontend/src/api/axios.tsx
@@ -4,13 +4,8 @@ import axios, { AxiosResponse } from 'axios';
 //GATEWAT DEVELOPMENT PORT IS 8212 - THIS SHOULD BE ACTIVE BY DEFAULT
 
 export const axiosClient = axios.create({
-<<<<<<< HEAD
-   baseURL: "http://localhost:1235", // Use this to test on your local machine, leave commented out.
-  //baseURL: 'http://3.235.74.191:8212/data',
-=======
-  //baseURL: "http://localhost:1235", // Use this to test on your local machine, leave commented out.
-  baseURL: 'http://3.235.74.191:8212/data',///AWS
->>>>>>> 6f8e16ea1fba85bce9efe1c2674991d650d1a845
+   //baseURL: "http://localhost:1235", // Use this to test on your local machine, leave commented out.
+  baseURL: 'http://3.235.74.191:8212/data',
   //if you don't have the following line, your login won't work
   withCredentials: false, // we should probably change this later
 });
