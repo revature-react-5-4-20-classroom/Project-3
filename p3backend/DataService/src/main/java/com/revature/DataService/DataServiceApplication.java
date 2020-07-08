a3328f991c7102ae4263548a6d55284b99dea6b8
Mark Alsip <mark.alsip@comcast.net>
2020-07-08 10:34:01 -0600

    more cors testing

--- p3backend/DataService/src/main/java/com/revature/DataService/DataServiceApplication.java
+++ p3backend/DataService/src/main/java/com/revature/DataService/DataServiceApplication.java
@@ -23,8 +23,8 @@ public class DataServiceApplication {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
-            .allowedOrigins("127.0.0.1");
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*");
       }
     };
   }
