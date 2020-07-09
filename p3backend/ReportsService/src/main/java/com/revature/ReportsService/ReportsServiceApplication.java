c7cc70981f71b572be590fe5ec4a29d55a4337fa
Mark Alsip <mark.alsip@comcast.net>
2020-07-09 09:11:34 -0600

    fixing cors filter in reports

--- p3backend/ReportsService/src/main/java/com/revature/ReportsService/ReportsServiceApplication.java
+++ p3backend/ReportsService/src/main/java/com/revature/ReportsService/ReportsServiceApplication.java
@@ -24,8 +24,8 @@ public class ReportsServiceApplication {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*");
 
       }
     };
